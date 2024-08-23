37c7fbe408a95e79cc6ad8d639055585f6c26c46
yashodhara <yashodharagowdan@gmail.com>
2024-08-23 09:24:25 +0530

    new recipe

---
+++ src/main/java/com/example/demo/UseDiamondOp.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UseDiamondOp {
+    //uses diamond operator to declare the object
+
+    List<String> list = new ArrayList<String>();
+
+    Map<String, List<Integer>> map = new HashMap<String, List<Integer>>();
+}
